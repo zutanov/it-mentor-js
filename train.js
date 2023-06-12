06e20a3d93cf3618670d3e109f7d74f36a861147
Zamir Utanov <zamit.utanov@gmail.com>
2023-06-12 21:04:51 +0600

    'add promise task'

--- train.js
+++ train.js
@@ -189,3 +189,26 @@ async function f() {
 }
 f();
 console.log("1");
+
+Promise.resolve()
+  .then(() => console.log("a: 1"))
+  .then(() => {
+    setTimeout(() => console.log("timeout 2"));
+    console.log("a: 2");
+  })
+  .then(() => {
+    setTimeout(() => console.log("timeout 3"));
+    console.log("a: 3");
+  });
+
+new Promise((res, rej) => {
+  console.log("b");
+  rej(new Error("123"));
+})
+  .then(console.log("b 1"))
+  .then(
+    () => console.log("b 2"),
+    () => console.log("b")
+  )
+  .catch(() => console.log("b 3"))
+  .then(() => console.log("b 4"));
